3ad03db03d512873d3e227fda55e207dd574f441
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-06-27 14:22:26 +0200

    Temp test

--- kde_classes.py
+++ kde_classes.py
@@ -88,8 +88,8 @@ class Model(object):
             # print(np.sum(self.mc['conv']) * np.pi * 1e7)
             # print(np.sum(diff_weight) * np.pi * 1e7)
         else:
-            self.weights = np.ones(len(self.mc))
-            print('Using ones as weight.')
+            self.weights = np.ones(len(self.mc))*range(len(self.mc))
+            print('Using ones as weight. Multiply by range.')
 
 
 class KDE(object):
