47dfc2bc61cb446d1abe12163ad69e8853f57d39
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-07-09 12:59:37 +0200

    Create a model

---
+++ models/E_sig.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+settings = {
+    'logSigma': {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': 0.08,
+        'nbins': 100,
+        'range': [0.0, 1.0]
+        },
+    'logEr': {
+        'values': "mc['logE']",
+        'bandwidth': 0.15,
+        'nbins': 100,
+        'range': [0, 10]
+        }
+}
+
+#grid = np.linspace(0, 1, 1000)
+grid = None
