90575ef0afe172dea7964422b7e7096f07148809
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-07-09 14:14:28 +0200

    Bandwidth should be a list

--- models/E_sig.py
+++ models/E_sig.py
@@ -5,13 +5,13 @@ import numpy as np
 settings = {
     'logSigma': {
         'values': "np.log10(mc['sigma_pull_corrected'])",
-        'bandwidth': 0.08,
+        'bandwidth': [0.08],
         'nbins': 100,
         'range': [0.0, 1.0]
         },
     'logEr': {
         'values': "mc['logE']",
-        'bandwidth': 0.15,
+        'bandwidth': [0.15, 0.20],
         'nbins': 100,
         'range': [0, 10]
         }
