f34db4ee95180999ec7d3d41d7a02d364206d0f8
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-07-10 12:51:30 +0200

    Add slurm submission file

--- models/multi_gaussian.py
+++ models/multi_gaussian.py
@@ -5,13 +5,15 @@ import numpy as np
 settings = {
     'x': {
         'values': "mc['x']",
-        'bandwidth': np.linspace(0.1, 1, 10),
+        #'bandwidth': np.linspace(0.1, 1, 10),
+        'bandwidth': [0.3],
         'nbins': 100,
         'range': [-5, 5]
         },
     'y': {
         'values': "mc['y']",
-        'bandwidth': [0.3, 0.4, 0.5, 0.6],
+        #'bandwidth': [0.3, 0.4, 0.5, 0.6],
+        'bandwidth': [0.3],
         'nbins': 100,
         'range': [-5, 5]
         }
---
+++ slurm.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import itertools
+import pickle
+
+from config import CFG
+from dataset import load_and_prepare_data
+from kde_classes import Model, KDE
+
+from models.multi_gaussian import settings, grid
+
+mc = np.load(CFG['paths']['mg_mc'])
+
+model = Model(mc, settings, weighting=None)
+
+kde = KDE(model)
+
+kde_dump_file = open('kde_dump_file', 'wb')
+pickle.dump(kde, kde_dump_file)
+kde_dump_file.close()
+
+# slurm_draft = '#!/usr/bin/env bash \n\
+# #SBATCH --time=2:30:00 \n\
+# #SBATCH --mem=8000 \n\
+# #SBATCH --partition=kta \n\
+# #SBATCH --error={bpath}/slurm.err \n\
+# #SBATCH --output={bpath}/slurm.out \n\
+# bash ./env.sh {args}\n'
+
+slurm_draft = """# -*- coding: utf-8 -*-
+
+import pickle
+
+kde = pickle.load('kde_dump_file')
+
+result = kde.cross_validate({bandwidth}, {adaptive})
+
+f = open("/var/tmp/dict_test.txt","w")
+f.write(str(result))
+f.close()
+"""
+
+for bandwidth in itertools.product(*self.model.bandwidths):
+    adaptive = False
+    ex_slurm = slurm_draft.format(bpath=outfolder, bandwidth=bandwidth,
+                                  adaptive=adaptive)
+    temp_submit = 'temp_submit.sub'
+    with open(temp_submit, "wc") as file:
+        file.write(ex_slurm)
+
+    os.system("sbatch {}".format(temp_submit))
