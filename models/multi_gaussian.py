06cf81a39dc155fd706d513c882aa1739bf79808
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-07-11 12:40:35 +0200

    Use OrderedDict

--- models/multi_gaussian.py
+++ models/multi_gaussian.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from collections import OrderedDict
 
-settings = {
-    'x': {
-        'values': "mc['x']",
-        'bandwidth': np.linspace(0.1, 1, 10),
-        'nbins': 100,
-        'range': [-5, 5]
-        },
-    'y': {
-        'values': "mc['y']",
-        'bandwidth': [0.3, 0.4, 0.5, 0.6],
-        'nbins': 100,
-        'range': [-5, 5]
-        }
-}
+settings = OrderedDict([
+    ('x', {'values': "mc['x']",
+           'bandwidth': np.linspace(0.1, 1, 10),
+           'nbins': 100,
+           'range': [-5, 5]
+           }),
+    ('y': {'values': "mc['y']",
+           'bandwidth': [0.3, 0.4, 0.5, 0.6],
+           'nbins': 100,
+           'range': [-5, 5]
+           })
+])
 
 #grid = np.linspace(0, 1, 1000)
 grid = None
