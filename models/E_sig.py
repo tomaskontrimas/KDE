0fc9da5e7becd41e6a29978c1b21c0fa78a4e928
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-07-16 16:42:42 +0200

    Set names as MC field name

--- models/E_sig.py
+++ models/E_sig.py
@@ -4,13 +4,13 @@ import numpy as np
 from collections import OrderedDict
 
 settings = {
-    'logSigma': {
+    'sigma_pull_corrected': {
         'values': "np.log10(mc['sigma_pull_corrected'])",
         'bandwidth': [0.08],
         'nbins': 200,
         'range': None
         },
-    'logEr': {
+    'logE': {
         'values': "mc['logE']",
         'bandwidth': [0.15],
         'nbins': 100,
