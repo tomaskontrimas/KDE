df30be1d71119308e4e79b4bf8a5c36018804ecd
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-07-18 19:22:33 +0200

    Change model nbins

--- models/sig_E.py
+++ models/sig_E.py
@@ -7,13 +7,13 @@ settings = OrderedDict([
     ('lgb_sigma_psimu', {
         'values': "np.log10(mc['lgb_sigma_psimu'])",
         'bandwidth': np.linspace(0.2, 0.06, 5), #[0.04]
-        'nbins': 200,
+        'nbins': 100,
         'range': None
         }),
     ('log_e', {
         'values': "mc['log_e']",
         'bandwidth': np.linspace(0.16, 0.20, 5), #[0.18]
-        'nbins': 200,
+        'nbins': 100,
         'range': None
         })
 ])
