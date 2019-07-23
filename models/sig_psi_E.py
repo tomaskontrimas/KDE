3b9badeccad25ba58994ecb528025db2a1d68fa5
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-07-23 15:39:55 +0200

    Change bandwidths range

--- models/sig_psi_E.py
+++ models/sig_psi_E.py
@@ -6,19 +6,19 @@ from collections import OrderedDict
 settings = OrderedDict([
     ('sigma_pull_corrected', {
         'values': "np.log10(mc['sigma_pull_corrected'])",
-        'bandwidth': np.linspace(0.01, 0.11, 7), #[0.06]
+        'bandwidth': np.linspace(0.05, 0.25, 6), #[0.06]
         'nbins': 100,
         'range': None
         }),
     ('psi_mu', {
         'values': "np.log10(mc['psi_mu'])",
-        'bandwidth': np.linspace(0.10, 0.22, 7), #[0.16]
+        'bandwidth': np.linspace(0.10, 0.30, 6), #[0.16]
         'nbins': 400,
         'range': None
         }),
     ('log_e', {
         'values': "mc['log_e']",
-        'bandwidth': np.linspace(0.05, 0.25, 11), #[0.12]
+        'bandwidth': np.linspace(0.10, 0.30, 6), #[0.12]
         'nbins': 100,
         'range': None
         })
