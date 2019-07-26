05a2520d788bd52697a6535f60a107a05dd659a4
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-07-26 15:54:37 +0200

    Improve docstrings and fix minor issues

--- dataset.py
+++ dataset.py
@@ -4,12 +4,21 @@ from config import CFG
 import numpy as np
 from numpy.lib import recfunctions as np_rfn
 import os.path
-import sys
 
 def load_and_prepare_data(pathfilenames):
-    """Loads the data and renames fields.
+    """Loads the data file(s), renames fields and applies diffuse dataset cuts.
+
+    Parameters
+    ----------
+    pathfilenames : str | sequence of str
+        The file name(s), including path(s), of the monte-carlo data file(s).
+
+    Returns
+    -------
+    data : numpy record ndarray
+        Loaded and prepared monte-carlo data.
     """
-    pathfilenames = [pathfilenames]
+    pathfilenames = list(pathfilenames)
     pathfilename = pathfilenames[0]
     assert_file_exists(pathfilename)
     data = np.load(pathfilename)
@@ -21,20 +30,43 @@ def load_and_prepare_data(pathfilenames):
     # Rename fields based on MC_keys dictionary.
     data = np_rfn.rename_fields(data, CFG['MC_keys'])
 
-    # Apply diffuse dataset cut.
+    # Apply diffuse dataset cuts.
     data = diffuse_cuts(data)
 
     return data
 
 def diffuse_cuts(mc):
+    """Applies diffuse dataset cuts on a given monte-carlo data.
+
+    Parameters
+    ----------
+    mc : str | numpy record ndarray
+        Monte-carlo data.
+
+    Returns
+    -------
+    mc_dc : numpy record ndarray
+        Monte-carlo data after diffuse dataset cuts.
+    """
     log_true_e = np.log10(mc['true_energy'])
-    return mc[(mc['true_dec'] > np.radians(-5)) &
-              (np.log10(mc['true_energy']) < 8.0) &
-              (mc['sigmaok'] == 0)]
+    mc_dc = mc[(mc['true_dec'] > np.radians(-5)) &
+               (np.log10(mc['true_energy']) < 8.0) &
+               (mc['sigmaok'] == 0)]
+    return mc_dc
 
 def assert_file_exists(pathfilename):
-    """Checks if the given file exists and raises a RuntimeError if it does
-    not exist.
+    """Checks if the given file exists.
+
+    Parameters
+    ----------
+    pathfilenames : str
+        The file name, including path.
+
+    Raises
+    ------
+    RuntimeError
+        If the file does not exist.
+
     """
     if(not os.path.isfile(pathfilename)):
         raise RuntimeError('The data file "%s" does not exist!'%(pathfilename))
