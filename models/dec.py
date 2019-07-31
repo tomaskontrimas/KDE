6b0cf5d48a90a4d62f5273297f3c756c8716975e
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-07-31 12:35:28 +0200

    Bandwidth close up around max LLH

--- models/dec.py
+++ models/dec.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 settings = OrderedDict([
     ('true_dec', {
         'values': "np.sin(mc['true_dec'])",
-        'bandwidth': np.linspace(0.005, 0.05, 9),
+        'bandwidth': np.linspace(0.005, 0.015, 11),
         'nbins': 400,
         'range': [np.sin(np.radians(-5)), 1.0]
         })
