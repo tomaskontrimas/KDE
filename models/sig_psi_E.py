36d94db5a84ef95a4cc6be21f07d2be7520a64bb
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-08-12 10:20:05 +0200

    Update bandwidths

--- models/sig_psi_E.py
+++ models/sig_psi_E.py
@@ -32,22 +32,19 @@ settings['default'] = OrderedDict([
 settings['1.5'] = OrderedDict([
     ('sigma_pull_corrected', {
         'values': "np.log10(mc['sigma_pull_corrected'])",
-        'bandwidth': np.concatenate((np.linspace(0.11, 0.13, 2),
-                                     np.linspace(0.17, 0.19, 2))),
+        'bandwidth': np.linspace(0.13, 0.21, 5),
         'nbins': 150,
         'range': None
         }),
     ('psi', {
         'values': "np.log10(mc['psi'])",
-        'bandwidth': np.concatenate((np.linspace(0.40, 0.42, 2),
-                                     np.linspace(0.46, 0.48, 2))),
+        'bandwidth': np.linspace(0.38, 0.46, 5),
         'nbins': 300,
         'range': None
         }),
     ('log_e', {
         'values': "mc['log_e']",
-        'bandwidth': np.concatenate((np.linspace(0.52, 0.54, 2),
-                                     np.linspace(0.58, 0.60, 2))),
+        'bandwidth': np.linspace(0.58, 0.66, 5),
         'nbins': 150,
         'range': None
         })
@@ -56,22 +53,19 @@ settings['1.5'] = OrderedDict([
 settings['2.0'] = OrderedDict([
     ('sigma_pull_corrected', {
         'values': "np.log10(mc['sigma_pull_corrected'])",
-        'bandwidth': np.concatenate((np.linspace(0.07, 0.09, 2),
-                                     np.linspace(0.13, 0.15, 2))),
+        'bandwidth': np.linspace(0.09, 0.17, 5),
         'nbins': 150,
         'range': None
         }),
     ('psi', {
         'values': "np.log10(mc['psi'])",
-        'bandwidth': np.concatenate((np.linspace(0.26, 0.28, 2),
-                                     np.linspace(0.32, 0.34, 2))),
+        'bandwidth': np.linspace(0.32, 0.40, 5),
         'nbins': 300,
         'range': None
         }),
     ('log_e', {
         'values': "mc['log_e']",
-        'bandwidth': np.concatenate((np.linspace(0.18, 0.20, 2),
-                                     np.linspace(0.24, 0.26, 2))),
+        'bandwidth': np.linspace(0.24, 0.32, 5),
         'nbins': 150,
         'range': None
         })
