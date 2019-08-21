d09c5e7b24d4f65e80b8b12065aa2a45ca1b307e
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-08-21 22:16:30 +0200

    Update sig_dec_E model

--- models/sig_dec_E.py
+++ models/sig_dec_E.py
@@ -3,10 +3,13 @@
 import numpy as np
 from collections import OrderedDict
 
-settings = OrderedDict([
+settings = {}
+
+settings['default'] = OrderedDict([
     ('sigma_pull_corrected', {
         'values': "np.log10(mc['sigma_pull_corrected'])",
-        'bandwidth': np.linspace(0.05, 0.25, 6),
+        'bandwidth': np.concatenate((np.linspace(0.11, 0.13, 2),
+                                     np.linspace(0.17, 0.19, 2))),
         'nbins': 100,
         'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
                    np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
@@ -14,7 +17,272 @@ settings = OrderedDict([
     ('true_dec', {
         'values': "np.sin(mc['true_dec'])",
         'bandwidth': np.linspace(0.10, 0.60, 6),
-        'nbins': 400,
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.concatenate((np.linspace(0.52, 0.54, 2),
+                                     np.linspace(0.58, 0.60, 2))),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['1.5'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.40, 0.90, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.0'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.1'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.2'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.3'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.4'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.5'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.6'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.7'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.8'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['2.9'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        }),
+    ('log_e', {
+        'values': "mc['log_e']",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
+        'range': "[np.min(mc['log_e'])-0.5, np.max(mc['log_e'])+0.5]"
+        })
+])
+
+settings['3.0'] = OrderedDict([
+    ('sigma_pull_corrected', {
+        'values': "np.log10(mc['sigma_pull_corrected'])",
+        'bandwidth': np.linspace(0.05, 0.30, 6),
+        'nbins': 100,
+        'range': "[np.min(np.log10(mc['sigma_pull_corrected']))-0.5, \
+                   np.max(np.log10(mc['sigma_pull_corrected']))+0.5]"
+        }),
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.10, 0.60, 6),
+        'nbins': 100,
         'range': [np.sin(np.radians(-5)), 1.0]
         }),
     ('log_e', {
