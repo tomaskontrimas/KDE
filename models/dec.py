6a3fa9acc647f6a557f4d56c55f86f7ce8d4f113
Tomas Kontrimas <t.kontrimas@outlook.com>
2019-08-26 15:27:38 +0200

    Update dec model

--- models/dec.py
+++ models/dec.py
@@ -3,11 +3,121 @@
 import numpy as np
 from collections import OrderedDict
 
-settings = OrderedDict([
+settings = {}
+
+settings['default'] = OrderedDict([
     ('true_dec', {
         'values': "np.sin(mc['true_dec'])",
-        'bandwidth': np.linspace(0.005, 0.015, 11),
-        'nbins': 400,
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['1.5'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.0'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.1'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.2'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.3'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.4'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.5'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.6'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.7'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.8'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['2.9'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
+        'range': [np.sin(np.radians(-5)), 1.0]
+        })
+])
+
+settings['3.0'] = OrderedDict([
+    ('true_dec', {
+        'values': "np.sin(mc['true_dec'])",
+        'bandwidth': np.linspace(0.005, 0.10, 20),
+        'nbins': 100,
         'range': [np.sin(np.radians(-5)), 1.0]
         })
 ])
