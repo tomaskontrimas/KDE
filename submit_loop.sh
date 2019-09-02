37d3c521b84d295d68f67be2526dd8c9d14ac20d
Kontrimas <ge56lag@nx1.ktas.ph.tum.de>
2019-09-02 10:54:36 +0200

    Checkpoint

--- submit_loop.sh
+++ submit_loop.sh
@@ -1,4 +1,4 @@
-for i in 1.5 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0
+for i in 1.5 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0 3.5
 do
     #python submit_cv.py --adaptive --split --seed --weighting=plotter_wkde --gamma=$i --phi0=1.0 sig_psi_E
     #python submit_cv.py --adaptive --split --weighting=plotter_wkde --gamma=$i --phi0=1.0 sig_psi_E
@@ -15,5 +15,8 @@ do
     #python submit_pdf.py --adaptive --weighting=plotter_wkde --gamma=$i --phi0=1.0 bg
     
     #python submit_cv.py --adaptive --split --weighting=plotter_wkde --gamma=$i --phi0=1.0 dec
-    python submit_pdf.py --adaptive --weighting=plotter_wkde --gamma=$i --phi0=1.0 dec
+    #python submit_pdf.py --adaptive --weighting=plotter_wkde --gamma=$i --phi0=1.0 dec
+
+    python submit_cv.py --adaptive --split --nbins=100 --weighting=conv+pl --gamma=$i --phi0=1.0 bg
+    #python submit_pdf.py --adaptive --weighting=conv+pl --gamma=$i --phi0=1.0 bg
 done
